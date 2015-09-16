4f62f643da1b989ff3dc0e49c6231cbf8fdee1f3
wrist <stoicheia1986@gmail.com>
2015-09-16 15:12:52 +0900

    implemented double buffer

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,93 @@
+"use strict";
+
 document.addEventListener( "DOMContentLoaded", function () {
         var getImageList = function (self) {
             // get image list
-            $.ajax({
+            return $.ajax({
                     url: "http://localhost:3000/images"
             }).then(function(json){
                     var images = json["images"];
                     console.log(images);
-                    self.images = images;
-            }).done(function () {
-                    self.image = self.images[self.index];
-                    self.message = self.image;
+                    self.image_list = images;
             });
-        }
+        };
+
+        var createImageWithOnload = function (max_width, max_height) {
+            var img = new Image();
+            img.onload = function () {
+                var rate = 1.0;
+                var width  = img.width;
+                var height = img.height;
+
+                img.style.width = 'auto';
+                img.style.height = 'auto';
+
+                // adjust height to fit height
+                rate = max_height / img.height;
+                width  *= rate;
+                height *= rate;
+
+                // if width is over inner window, resize again
+                if( width > max_width ){
+                    rate = max_width / img.width;
+                    width  *= rate;
+                    height *= rate;
+                }
+
+                // set size
+                img.width  = width;
+                img.height = height;
+
+                // clear style
+                img.style.width = '';
+                img.style.height = '';
+            }
+            return img;
+        };
 
         var vm = new Vue({
                 el: '#slideshow',
                 data: {
                     message: '',
-                    images: [],
-                    image: "",
-                    index: 0,
+                    image_list: [],
+                    ab_buffer: [undefined, undefined],
+                    ab_index: 0,
+                    image_index: 0,
                     max_width:  window.innerWidth,
                     max_height: window.innerHeight
                 },
                 created: function(){
-                    getImageList(this);
-                },
-                methods: {
-                    resize: function () {
-                        var img = $("#slideshow img");
-                        img.css('width', 'auto');
-                        img.css('height', 'auto');
-
-                        var rate = 1.0;
-                        var width  = img.width();
-                        var height = img.height();
-
-                        // adjust height to fit height
-                        rate = this.max_height / height;
-                        width  *= rate;
-                        height *= rate;
-
-                        // if width over inner window, resize again
-                        if( width > this.max_width ){
-                            rate = this.max_width / width;
-                            width  *= rate;
-                            height *= rate;
-                        }
-
-                        // set size
-                        img.width(width);
-                        img.height(height);
-                    }
+                    var self = this;  // bind
+                    getImageList(this).done(function () {
+                            var img = createImageWithOnload(self.max_width,
+                                                            self.max_height);
+                            img.src = self.image_list[self.image_index];
+                            self.ab_buffer[0] = img;
+                    });
                 }
         });
 
         // update images
         setInterval(function () {
-                if( vm.index === (vm.images.length - 1) ){
-                    vm.index = 0;
-                } else {
-                    vm.index++;
-                }
+                var next_ab_index = (vm.ab_index == 0) ? 1 : 0;
+                var img = createImageWithOnload(vm.max_width, vm.max_height);
 
-                vm.image = vm.images[vm.index];
-                vm.message = vm.image;
+                // set image path and pre-load
+                vm.image_index += 1;
+                vm.image_index = vm.image_index % vm.image_list.length;
+                img.src = vm.image_list[vm.image_index];
+
+                vm.ab_buffer[next_ab_index] = img;
+
+                // remove and append images
+                $("#slideshow img").remove();
+                $("#slideshow").append(vm.ab_buffer[vm.ab_index]);
+
+                // update message
+                vm.message = vm.ab_buffer[vm.ab_index].src.split("/").pop();
+
+                // toggle ab_index
+                vm.ab_index = (vm.ab_index == 0) ? 1 : 0;
             },
             4000
         );
